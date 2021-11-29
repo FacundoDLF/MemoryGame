add7843ac4e7c5007ee9ce8aa60911878fafc7e8
Facu DLF <facundo.delafuente@radiumrocket.com>
2021-11-29 12:19:46 -0300

    Triying to start..

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+import Vue from 'vue';
+
+const App = Vue.extend ({
+  name: 'App',
+  components: {
+
+  }
+});
+
+export default App
+</script>
+
+<style>
+</style>
