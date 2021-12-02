12abace6b0928c7a4572d7c7c91898d02af01271
Facu DLF <facundo.delafuente@radiumrocket.com>
2021-12-02 12:15:34 -0300

    Add cards link route.

---
+++ src/router/cards.js
@@ -0,0 +1,40 @@
+import REVERSECARD from '../assets/img/reverseCard.png';
+import CARD_1 from '../assets/img/aladinMonkey.png';
+import CARD_2 from '../assets/img/athenianSmokingMonkey.png';
+import CARD_3 from '../assets/img/blueBandanaMonkey.png';
+import CARD_4 from '../assets/img/blueMarineMonkey.png';
+import CARD_5 from '../assets/img/coinEyesMonkey.png';
+import CARD_6 from '../assets/img/highEyesMonkey.png';
+import CARD_7 from '../assets/img/highHolyMonkey.png';
+import CARD_8 from '../assets/img/jailSmokingMonkey.png';
+import CARD_9 from '../assets/img/karateMonkey.png';
+import CARD_10 from '../assets/img/marineHighEyesMonkey.png';
+import CARD_11 from '../assets/img/marineMonkey.png';
+import CARD_12 from '../assets/img/whiteMarineMonkey.png';
+import CARD_13 from '../assets/img/athenianRobotMonkey.png';
+import CARD_14 from '../assets/img/karate3DMonkey.png';
+import CARD_15 from '../assets/img/smokingRobotMonkey.png';
+import CARD_16 from '../assets/img/sunglassesRobotMonkey.png';
+import CARD_17 from '../assets/img/smokingSunglassesMonkey.png';
+
+export default {
+    [REVERSECARD]: 'REVERSECARD',
+    [CARD_1]: 'CARD_1',
+    [CARD_2]: 'CARD_2',
+    [CARD_3]: 'CARD_3',
+    [CARD_4]: 'CARD_4',
+    [CARD_5]: 'CARD_5',
+    [CARD_6]: 'CARD_6',
+    [CARD_7]: 'CARD_7',
+    [CARD_8]: 'CARD_8',
+    [CARD_9]: 'CARD_9',
+    [CARD_10]: 'CARD_10',
+    [CARD_11]: 'CARD_11',
+    [CARD_12]: 'CARD_12',
+    [CARD_13]: 'CARD_13',
+    [CARD_14]: 'CARD_14',
+    [CARD_15]: 'CARD_15',
+    [CARD_16]: 'CARD_16',
+    [CARD_17]: 'CARD_17',
+
+}
