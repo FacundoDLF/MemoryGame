0f61ee42fd7babd4fa2004b08647d35dbeed20d7
Facu DLF <facundo.delafuente@radiumrocket.com>
2021-12-06 13:30:43 -0300

    Fix logic to showCards and match couples.

--- src/components/CardLayout.vue
+++ src/components/CardLayout.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+    <div>
+      <select name="levels" id="levels">
+        <option value="x4">Level x 4</option>
+        <option value="x8">Level x 8</option>
+        <option value="x16">Level x 12</option>
+        <option value="x32">Level x 16</option>
+      </select>
+    </div>
     <button
       class="card"
       v-for="card in cards"
@@ -28,14 +36,17 @@ import CARD_1 from "../assets/img/aladinMonkey.png";
 import CARD_2 from "../assets/img/athenianSmokingMonkey.png";
 import CARD_3 from "../assets/img/blueBandanaMonkey.png";
 import CARD_4 from "../assets/img/blueMarineMonkey.png";
+
 import CARD_5 from "../assets/img/coinEyesMonkey.png";
 import CARD_6 from "../assets/img/highEyesMonkey.png";
 import CARD_7 from "../assets/img/highHolyMonkey.png";
 import CARD_8 from "../assets/img/jailSmokingMonkey.png";
+
 import CARD_9 from "../assets/img/karateMonkey.png";
 import CARD_10 from "../assets/img/marineHighEyesMonkey.png";
 import CARD_11 from "../assets/img/marineMonkey.png";
 import CARD_12 from "../assets/img/whiteMarineMonkey.png";
+
 import CARD_13 from "../assets/img/athenianRobotMonkey.png";
 import CARD_14 from "../assets/img/karate3DMonkey.png";
 import CARD_15 from "../assets/img/smokingRobotMonkey.png";
@@ -55,7 +66,7 @@ export default {
         },
         {
           id: 2,
-          src: CARD_1,
+          src: CARD_2,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
@@ -76,168 +87,168 @@ export default {
         },
         {
           id: 5,
-          src: CARD_5,
+          src: CARD_1,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
           id: 6,
-          src: CARD_6,
+          src: CARD_2,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
           id: 7,
-          src: CARD_7,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 8,
-          src: CARD_8,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 9,
-          src: CARD_9,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 10,
-          src: CARD_10,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 11,
-          src: CARD_11,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 12,
-          src: CARD_12,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 13,
-          src: CARD_13,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 14,
-          src: CARD_14,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 15,
-          src: CARD_15,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 16,
-          src: CARD_16,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 17,
-          src: CARD_2,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 18,
-          src: CARD_2,
-          alt: "Monkey Card",
-          isReversed: false,
-          isBlocked: false,
-        },
-        {
-          id: 19,
           src: CARD_3,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
-          id: 20,
+          id: 8,
           src: CARD_4,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
-          id: 21,
+          id: 9,
           src: CARD_5,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
-          id: 22,
+          id: 10,
           src: CARD_6,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
-          id: 23,
+          id: 11,
           src: CARD_7,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
-          id: 24,
+          id: 12,
           src: CARD_8,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
-          id: 25,
+          id: 13,
+          src: CARD_5,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 14,
+          src: CARD_6,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 15,
+          src: CARD_7,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 16,
+          src: CARD_8,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 17,
           src: CARD_9,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
-          id: 26,
+          id: 18,
           src: CARD_10,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
-          id: 27,
+          id: 19,
           src: CARD_11,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
         },
         {
-          id: 28,
+          id: 20,
           src: CARD_12,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 21,
+          src: CARD_9,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 22,
+          src: CARD_10,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 23,
+          src: CARD_11,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 24,
+          src: CARD_12,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 25,
+          src: CARD_13,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 26,
+          src: CARD_14,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 27,
+          src: CARD_15,
+          alt: "Monkey Card",
+          isReversed: false,
+          isBlocked: false,
+        },
+        {
+          id: 28,
+          src: CARD_16,
           alt: "Monkey Card",
           isReversed: false,
           isBlocked: false,
@@ -282,7 +293,9 @@ export default {
       );
         Vue.set(this.cards, index, { ...clickedCard, isBlocked: true });
         Vue.set(this.cards, indexOfRM, { ...this.reversedMatch, isBlocked: true });
-        // this.reversedMatch = null; --> No lo se Rick ...
+        Vue.set(this.cards, index, { ...clickedCard, isReversed: true });
+        Vue.set(this.cards, indexOfRM, { ...this.reversedMatch, isReversed: true });
+        // this.reversedMatch = null; // No lo se Rick ...
     },
     showAndHide(clickedCard, index) {
       const indexOfRM = this.cards.findIndex(
@@ -313,6 +326,7 @@ export default {
         // Si: Ver si coincide
         if (clickedCard.src === this.reversedMatch.src) {
           this.blockCouple(clickedCard, indexOfCard);
+          this.reversedMatch = null;
         } else {
           this.showAndHide(clickedCard, indexOfCard);
         }
